ac57ae1c4db4d9c3a658cc152735654329f1d201
Rajiv Yadav <rajiv.yadav@globallogic.com>
2023-10-12 20:58:55 +0530

    commiting Workspace3 of STS

--- BookStore 2/src/main/java/com/globallogic/BookStore/Repository/Repo.java
+++
@@ -1,9 +0,0 @@
-package com.globallogic.BookStore.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.globallogic.BookStore.Entity.Book;
-
-public interface Repo extends JpaRepository<Book, String>{
-
-}
